22bb8cce18161810655b0b96b3a04efb9b28ef8b
Jeroen Bekker <Jeroen.Bekker@stedin.net>
2018-11-02 08:46:23 +0100

    first script

---
+++ Set-Random-WindowsWallpaper.ps1
@@ -0,0 +1,25 @@
+﻿<# 
+.SYNOPSIS
+    A script to override the wallpaper set by policy
+#>
+
+[CmdletBinding(SupportsShouldProcess=$True)]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$imageFolderPath 
+)
+try {
+    $dir = get-item "$imageFolderPath"
+    $supportedFileTypes = @(".jpg",".jpeg",".png",".tiff",".tif")    
+    $PictureSet = Get-ChildItem -Path $dir | Where-Object {$_.Extension -in $supportedFileTypes}
+
+    $imageFullPath = $pictureSet[(Get-Random -maximum $PictureSet.Length)].FullName
+    Write-Output "the selected image is: $imageFullPath"
+    Copy-Item "$imageFullPath" -destination "$env:APPDATA\Microsoft\Windows\Themes\TranscodedWallpaper" -Force -errorAction Stop
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 
+}
+catch {
+    Throw $_.Exception.Message 
+}
---
+++ Set-WindowsWallpaper.ps1
@@ -0,0 +1,20 @@
+﻿<# 
+.SYNOPSIS
+    A script to override the wallpaper set by policy
+#>
+
+[CmdletBinding(SupportsShouldProcess=$True)]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$imageFullPath 
+)
+try {
+    
+    Copy-Item "$imageFullPath" -destination "$env:APPDATA\Microsoft\Windows\Themes\TranscodedWallpaper" -Force -errorAction Stop
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 
+}
+catch {
+    Throw $_.Exception.Message 
+}
